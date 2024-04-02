b252dc367bd94341dd4d9a1ef249b19807463c6f
ahmedmtaham <165436145+ahmedmtaham@users.noreply.github.com>
2024-04-02 07:55:33 -0700

    Add files via upload

--- name.js
+++ name.js
@@ -1,17 +1,19 @@
+const validNames = ["deema tamer", "loai tamer"];
+const error = document.querySelector('.error'); // Selects first element with the class
+
 // Get a reference to the form
 const form = document.getElementById('myForm');
 
 // Add a listener for the form's submit event
 form.addEventListener('submit', (event) => {
-    event.preventDefault(); // Prevent default form submission
+  event.preventDefault(); // Prevent default form submission
 
-    // Get the value of the first input element
-    const input = document.getElementsByClassName('input')[0].value;
+  const input = document.getElementsByClassName('input')[0].value.toLowerCase();
 
-    // Check the input value
-    if (input === "Deema Tamer" || input === "Loai Tamer") {
-        window.location.href = "verify.html";
-    } else {
-        console.warn("Invalid name. Access denied.");
-    }
+  if (validNames.includes(input)) {
+    window.location.href = "verify.html";
+  } else {
+    console.warn("Invalid name. Access denied.");
+    error.style.display = "block";
+  }
 });
